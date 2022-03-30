e5adfb05dff3b89dff7da8a1311b32252eeb049f
HEEJUNG956 <jungheesuk95@naver.com>
2022-03-30 14:34:19 +0900

    작은수제거하기

---
+++ 2페이지 level 1 5번 - 작은 수 제거하기/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_5번___작은_수_제거하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 3, 2, 1 };
+            int v = arr.Min();
+            int[] answer = arr.Where(x => x != v).ToArray();
+
+            if(answer.Count() == 0)
+            {
+                answer = new int[] { -1 };
+            }
+            for(int i = 0; i < answer.Length; i++)
+            {
+                Console.WriteLine(answer[i]);
+            }
+        }
+    }
+}
