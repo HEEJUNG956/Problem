46a2fa624a0f753428777111067ad8d9ab5f85df
HEEJUNG956 <jungheesuk95@naver.com>
2022-03-30 11:54:04 +0900

    주석추가

--- 2페이지 level 1 1번 - 평균구하기/Program.cs
+++ 2페이지 level 1 1번 - 평균구하기/Program.cs
@@ -10,6 +10,7 @@ namespace _2페이지_level_1_1번___평균구하기
     {
         static void Main(string[] args)
         {
+            //정수를 담고 있는 배열 arr의 평균값을 return하는 함수
             int[] arr = {Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine())};
             Console.WriteLine(arr.Average());
         }
