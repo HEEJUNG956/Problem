d4336d13c4dd1a8b0693e9ff75d160ee4b3cf198
HEEJUNG956 <jungheesuk95@naver.com>
2022-03-30 12:03:07 +0900

    최대공약수최소공배수

---
+++ 2페이지 level 1 3번 - 최대공약소최소공배수/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_3번___최대공약소최소공배수
+{
+    class Solution
+    {
+        public int[] solutioin(int n, int m)
+        {
+            int abc = gcd(n, m);
+            int[] answer = new int[] {abc, n * m / abc};
+            return answer;
+        }
+
+        int gcd(int a, int b)
+        {
+            return (a % b == 0 ? b : (gcd(b, a % b)));
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Int32.Parse(Console.ReadLine());
+            int b = Int32.Parse(Console.ReadLine());
+            Solution solution = new Solution();
+            int[] answer = solution.solutioin(a, b);
+            
+
+        }
+    }
+}
