815d6fceeb4838ce39f50d32c83416ccb80a38d6
HEEJUNG956 <jungheesuk95@naver.com>
2022-03-30 11:02:38 +0900

    이름변경

---
+++ 3페이지 level 1 2번- x만큼 간격이 있는 n개의 숫자/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_level_1___x만큼_간격이_있는_n개의_숫자
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 정수 x와 자연수 n을 입력 받아, x부터 시작해 x씩 증가하는 숫자를 n개 지니는 리스트를 리턴해야 합니다. 
+            // n개의숫자 분기확인용
+            String[] s;
+            s = Console.ReadLine().Split(' ');
+
+            int x = Int32.Parse(s[0]);
+            int n = Int32.Parse(s[1]);
+
+
+            long[] answer = new long[n];
+            for (int i = 0; i < n; i++)
+            {
+                if (i == 0)
+                {
+                    Console.WriteLine(answer[i] = x);
+                }
+                else
+                {
+                    Console.WriteLine(answer[i] = x + answer[i - 1]);
+                }
+            }
+        }
+
+    }
+}
---
+++ 3페이지 level 1 5번- 하샤드 수/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Problem_level_1___하샤드_수
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*양의 정수 x가 하샤드 수이려면 x의 자릿수의 합으로 x가 나누어져야 합니다.*/
+            int x = Convert.ToInt32(Console.ReadLine());
+            int tmp = x;
+            int value = 0;
+            bool answer = false;
+            while(tmp > 0)
+            {
+                value += tmp % 10;
+                tmp = tmp / 10;
+            }
+
+            if(x % value == 0)
+            {
+                answer = true;
+            }
+
+            Console.WriteLine(answer);
+        }
+    }
+}
