8066a73ab09c5046508119bb509fa0e7e25690f0
HEEJUNG956 <jungheesuk95@naver.com>
2022-03-30 11:16:12 +0900

    평균구하기

---
+++ 2페이지 level 1 1번 - 평균구하기/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2페이지_level_1_1번___평균구하기
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = {Int32.Parse(Console.ReadLine()), Int32.Parse(Console.ReadLine())};
+            Console.WriteLine(arr.Average());
+        }
+    }
+}
